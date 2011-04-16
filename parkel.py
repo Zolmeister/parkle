ec3480185b32ddde4f00c20ce412a4dcb5d4b53c
Brad Zeis <bradzeis@gmail.com>
2011-04-16 12:59:46 -0500

    Initial commit.

---
+++ parkel.py
@@ -0,0 +1,21 @@
+## Parkel 0.0
+
+class Parkel:
+    def __init__(self):
+        pass
+    
+    def roll(n):
+        """Return list of n random numbers from 1 to 6."""
+        pass
+
+class Player:
+    def __init__(self):
+        self.score = 0;
+        self.kept = [];
+
+    def decide(self, dice):
+        """Return number of dice left to roll."""
+        pass
+
+class RealPlayer(Player):
+    pass
