6937df3d84160f6f367834657a05fad279cbadf7
Brad Zeis <bradzeis@gmail.com>
2011-04-16 22:54:19 -0500

    Defined final game logic

--- parkel.py
+++ parkel.py
@@ -6,11 +6,21 @@ class Parkel(object):
         self.players = []
     
     def roll(n):
-        """Return list of n random numbers from 1 to 6."""
+        """Return list of n  (1 <= n <= 6) random numbers from 1 to 6.
+        
+        Return:
+            [[value, number of value], ...]
+
+            [[3, 2], [4, 4]] == Pair of 3s, four 4s.
+        """
         pass
 
     def start_game(self):
         """Return winning player."""
+
+        if len(self.players) == 0:
+            return
+
         for p in players:
             p.game = self
         
@@ -23,26 +33,84 @@ class Parkel(object):
                     return p
     
     def turn(player):
-        
+        player.rolls = 0
+        player.kept = []
+        player.begin_turn()
+
+        n = 6;
+        while(1):
+            d = self.roll(n);
+            player.rolls += 1
+
+            if not self.points_possible(d):
+                player.kept = []
+                return
+
+            r = player.decide(d)
+
+            if r == 0:
+                break
+
+            ## Determine if newest kept-set is valid
+            ## If invalid, remove entire new set from kept
+            nk = player.kept[-1]
+            c = len(nk);
+
+            if not calculate_one_keptset(nk):
+                player.kept = player.kept[0:-1]
+                continue
+
+            n -= c
+            if n == 0:
+                n = 6
+            elif n < 0 or n > 6:
+                player.kept = []
+                return
 
         player.score += self.determine_points(player.kept)
     
-    def determine_points(kept):
+
+    def points_possible(self, dice):
+        pass
+
+    def determine_points(self, kept):
         """Return number of points represented."""
         pass
 
-class Player(object):
+    def calculate_one_keptset(self, kset):
+        """Calculate number of points for a single item in a kept set."""
+        return 0;
+
+class ParkelPlayer(object):
     def __init__(self):
         self.game = None
         self.name = ""
         self.score = 0;
-        self.kept = [];
+        self.kept = []; # [[1, 1], [3, 3, 3]] = Pair of 2s kept first roll, triple 3s kept second roll
+                        # Each sublist is called a "kept-set"
+        self.rolls = 0
+
+    def begin_turn(self):
+        """User-defined setup method for the beginning of a turn.
+        
+        No altering of kept should be done here, only pre-calculations.
+        """
+        pass
 
     def decide(self, dice):
-        """Return number of dice left to roll."""
-        return 0
+        """Append kept-set to kept, return 1 to continue with turn, 0 to stop.
+        
+        Do NOT:
+            Change any scores or roll counts
+            Call any methods of a Parkel instance
+        
+        """
+        pass
 
-class RealPlayer(Player):
+class RealPlayer(ParkelPlayer):
+    def begin_turn(self):
+        pass
+
     def decide(self, dice):
         pass
 
